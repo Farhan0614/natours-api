bd289989f018637a4a285704258c65e9e984e8d3
Farhan Mirza <farhanmirza0614@gmail.com>
2026-08-01 19:54:41 +0500

    Initial commit: basic server and tour models (production vs development errors)

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,38 @@
+const sendErrorDevelopment = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    err: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProduction = (err, res) => {
+  // operational trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or unknown error: don't leak information to the client
+  } else {
+    console.error('ERROR: ', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
 export const globalErrorHandler = (err, req, res, next) => {
   // console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDevelopment(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProduction(err, res);
+  }
 };
