961dd2ef20d3e09a9a58b44956eea8d9f2f36613
Farhan Mirza <farhanmirza0614@gmail.com>
2026-08-05 16:14:05 +0500

    authentication and authorization(implementing the forget functionalioty)

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -17,7 +17,7 @@ const userRouter = express.Router();
 userRouter.post('/signup', signUp);
 userRouter.post('/login', login);
 userRouter.post('/forgetPassword', forgetPassword);
-userRouter.post('/resetPassword', resetPassword);
+userRouter.patch('/resetPassword/:token', resetPassword);
 
 userRouter.route('/').get(getAllUsers).post(createUser);
 userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
