510b32f7794e5708ef9763c5a76d50e17a287744
Farhan Mirza <farhanmirza0614@gmail.com>
2026-08-06 20:19:30 +0500

    authentication and authorization(setting security headers using helmet )

--- app.js
+++ app.js
@@ -5,26 +5,35 @@ import userRouter from './Routes/userRoutes.js';
 import AppError from './utils/appError.js';
 import { globalErrorHandler } from './controllers/errorController.js';
 import { rateLimit } from 'express-rate-limit';
+import helmet from 'helmet';
 
 const app = express();
-
 // Global Middlewares
+
+// set security headers
+app.use(helmet());
+
+// set development environment
 app.set('query parser', 'extended');
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// limit the requests from same api
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   limit: 100,
   message: 'Too many requests from this IP, please try again in an hour.',
 });
-app.use(limiter);
+app.use('/api', limiter);
 
-app.use(express.json());
+// body parser reading the body from the request req.body
+app.use(express.json({ limit: '10kb' }));
 
+// serving static files
 app.use(express.static('./public'));
 
+// test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
