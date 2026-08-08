108fc3b6434e8cec7076d18ca1751d797bf02056
Farhan Mirza <farhanmirza0614@gmail.com>
2026-08-08 20:12:01 +0500

    Modelling Data and Advanced Mongoose(modeling tour guide embedding)

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import slugify from 'slugify';
+import User from './userModel.js';
 
 const tourSchema = new mongoose.Schema(
   {
@@ -101,6 +102,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -116,6 +118,12 @@ tourSchema.virtual('durationWeeks').get(function () {
 tourSchema.pre('save', function () {
   this.slug = slugify(this.name, { lower: true });
 });
+
+tourSchema.pre('save', async function () {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+});
+
 // tourSchema.pre('save', function () {
 //   console.log('will save document');
 // });
