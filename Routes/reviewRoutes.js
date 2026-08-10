cc24795c9dc2c01eda63aa3bd009847abea0c1f4
Farhan Mirza <farhanmirza0614@gmail.com>
2026-08-10 20:05:48 +0500

    Modelling Data and Advanced Mongoose(adding a nested get endpoint)

--- Routes/reviewRoutes.js
+++ Routes/reviewRoutes.js
@@ -7,6 +7,7 @@ import {
 
 const reviewRouter = express.Router({ mergeParams: true });
 
+// automatically set tour and user IDs for nested routes with the help of mergeParams: true in the router command above. This allows us to access the tourId from the parent route and set it in the request body for creating a review. Similarly, we can set the user ID from the authenticated user.
 reviewRouter
   .route('/')
   .get(getAllReviews)
