4299b688b000efd7a5e1e9406cb7557590372c0e
Farhan Mirza <farhanmirza0614@gmail.com>
2026-08-11 19:24:40 +0500

    Modelling Data and Advanced Mongoose(today push no 3)

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -16,15 +16,15 @@ export const getAllReviews = catchAsync(async (req, res, next) => {
   });
 });
 
-export const getReview = catchAsync(async (req, res, next) => {
-  const review = await Review.findById(req.params.id);
-  if (!review) {
-    return next(new AppError('No review found with that ID', 404));
-  }
-  res.status(200).json({
+export const createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  const newReview = await Review.create(req.body);
+  res.status(201).json({
     status: 'success',
     data: {
-      review,
+      review: newReview,
     },
   });
 });
@@ -41,4 +41,29 @@ export const createReview = catchAsync(async (req, res, next) => {
     },
   });
 });
-console.log('this is today push no 1');
+
+export const createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  const newReview = await Review.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview,
+    },
+  });
+});
+
+export const createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  const newReview = await Review.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview,
+    },
+  });
+});
